29a6607b9a0848fd61c3a82c66513986607a24ac
cwestphal213 <47224660+cwestphal213@users.noreply.github.com>
2019-04-22 20:12:56 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,5 @@
 
+
 var dataP = d3.json("classData.json")
 
 dataP.then(function(data){
@@ -6,17 +7,46 @@ dataP.then(function(data){
 
 var allPenR = make2DArray(data)
 
-console.log(allPenR)
+var width = 500
+var height = 500
+
+
+var svg = d3.select('svg')
+            .attr('width', width)
+            .attr('height', height);
+
+            var xScale = d3.scaleLinear()
+                            .domain([0,allPenR.length])
+                            .range([0,width]);
+
+            var yScale = d3.scaleLinear()
+                          .domain([0, allPenR.length])
+                          .range([height - height/23, 0]);
+
+
+
+            var plot = svg.append('g')
+                            .attr('width',width)
+                            .attr('hieght',height);
+
+                    allPenR.forEach(function(d,i){
+                                plot.selectAll('rect'+i)
+                                .data(allPenR[i])
+                                .enter()
+                                .append('rect')
+                                .attr('id',function(){return 'a'+i})
+                                .attr('x',function(da,ia){return xScale(ia)})
+                                .attr('y', function(db,ib){return yScale(i)})
+                                .attr('height',function(){return height/23})
+                                .attr('width',function(){return width/allPenR[0].length})
+                                .attr('fill', function(dc,ic){return d3.interpolateBlues(dc)})
+
+                            })
 
 },
 function(err){
   console.log(err)
 })
-
-
-
-
-
 
 
 
